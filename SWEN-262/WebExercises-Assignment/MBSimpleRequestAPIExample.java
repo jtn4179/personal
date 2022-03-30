6ca7985daa2e4097f117ba3de288d64564b8e143
JT Novitsky <jtn4179@rit.edu>
2022-03-30 17:20:13 -0400

    adding starter code for the webexercises assignment

---
+++ SWEN-262/WebExercises-Assignment/MBSimpleRequestAPIExample.java
@@ -0,0 +1,46 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class MBSimpleRequestAPIExample {
+    private static final String SEARCH_URL =
+            "https://musicbrainz.org/ws/2/recording?query=";
+
+    private static final String XML_FORMAT = "&fmt=xml";
+
+    private static final String JSON_FORMAT = "&fmt=json";
+
+    public static void main(String[] args) throws IOException {
+        // %20 used to encode spaces in URL
+        String query = "\"Carry%20on%20Wayward%20Son\"";
+
+        URL queryUrl = new URL(SEARCH_URL + query + XML_FORMAT);
+        HttpsURLConnection connection =
+                (HttpsURLConnection)queryUrl.openConnection();
+
+        // a meaningful user agent is required to avoid throttling
+        // DO NOT exceed more than 1 request per second
+        connection.setRequestProperty("User-Agent",
+                "SWEN-262 Semester Project (https://www.se.rit.edu)");
+
+        connection.connect();
+        InputStream input = connection.getInputStream();
+        int responseCode = connection.getResponseCode();
+        if(responseCode == 200) {
+            InputStreamReader iReader = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(iReader);
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } else {
+            System.out.println("Server responded with " +  responseCode +
+                    ", you may be being throttled. Try again shortly.");
+        }
+
+        input.close();
+    }
+}
